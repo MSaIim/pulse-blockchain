065b81d6d607d2745a71990d4e3d8a5f02757ba7
Mohammad Salim <mfsalim27@gmail.com>
2021-03-04 21:50:11 -0500

    Start initial block creation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/MSaIim/pulse-blockchain
+
+go 1.16
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"time"
+)
 
-func main() {
-	fmt.Println("Pulse Blockchain")
+type block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+func calculateHash(block block) string {
+	record := fmt.Sprint(block.Index) + block.Timestamp + fmt.Sprint(block.BPM) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(oldBlock block, bpm int) (block, error) {
+	var newBlock block
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = time.Now().String()
+	newBlock.BPM = bpm
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
 }
